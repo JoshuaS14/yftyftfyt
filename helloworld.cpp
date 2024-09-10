1d4ed182caf5955ae400534ea61e6d2b9681ed33
JoshuaS14 <162597120+JoshuaS14@users.noreply.github.com>
2024-09-10 10:44:59 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    float age = 20;
+    int topspeed = 205;
+    string car = "Porsche Carrera GT";
+    char = P; 
+    bool quick = 3.5; 
+
+}
